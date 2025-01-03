380a12a64e2b3679176bbbc669fc0acba8234d64
Ronit Gurung <lecrepes45@gmail.com>
2025-01-03 15:15:28 +0000

    working mysql init db and cleanup

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,7 +1,8 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import userRouter from './routes/userRoutes';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import { mysqlDB } from './db/database';
 
 const app: Express = express();
 const port = process.env.PORT || 3300;
@@ -11,6 +12,32 @@ app.use(cookieParser());
 
 app.use('/v1/users', userRouter);
 
-app.listen(port, () => {
+async function initializeDB() {
+  console.log('Connected!');
+  await mysqlDB.query('CREATE DATABASE IF NOT EXISTS full_stack_test');
+  await mysqlDB.query('USE full_stack_test');
+  await mysqlDB.query(
+    'CREATE TABLE IF NOT EXISTS users (id INT  PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL UNIQUE, password VARCHAR(255) NOT NULL, refresh_token VARCHAR(255), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP )'
+  );
+}
+
+app.listen(port, async () => {
   console.log(`[server]: Server is running at http://localhost:${port}sssddd`);
+  await initializeDB();
 });
+
+async function cleanup() {
+  try {
+    const connection = await mysqlDB.getConnection();
+    await connection.query('DROP TABLE IF EXISTS users');
+    await connection.query('DROP DATABASE IF EXISTS full_stack_test');
+    await connection.release();
+    console.log('SQL cleaned');
+    process.exit(0);
+  } catch (err) {
+    console.error('Cleanup failed:', err);
+    process.exit(1);
+  }
+}
+
+process.on('SIGINT', cleanup);
